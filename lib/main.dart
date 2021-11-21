8d7a7894ea80510d50a837370dfac1ea309a27a1
Youcef-Abdelliche <hy_abdelliche@esi.dz>
2021-11-21 07:23:46 +0100

    add app icon

--- lib/main.dart
+++ lib/main.dart
@@ -20,3 +20,32 @@ class MyApp extends StatelessWidget {
   }
 }
 
+class Splash extends StatelessWidget {
+  const Splash({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    bool lightMode =
+        MediaQuery.of(context).platformBrightness == Brightness.light;
+    return Scaffold(
+      backgroundColor:
+          lightMode ? const Color(0xffe1f5fe) : const Color(0xff042a49),
+      body: Center(
+          child: lightMode
+              ? Image.asset('assets/splash.png')
+              : Image.asset('assets/splash_dark.png')),
+    );
+  }
+}
+
+class Init {
+  Init._();
+  static final instance = Init._();
+
+  Future initialize() async {
+    // This is where you can initialize the resources needed by your app while
+    // the splash screen is displayed.  Remove the following example because
+    // delaying the user experience is a bad design practice!
+    await Future.delayed(const Duration(seconds: 3));
+  }
+}
